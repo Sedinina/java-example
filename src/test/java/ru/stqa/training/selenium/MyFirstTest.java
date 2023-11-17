c94b39a8d1b9e85fc9b3a29881d3b6158a38d76f
Sedinina <sedinina.k@yandex.ru>
2023-11-17 15:42:38 +0500

    Задание 1. Подготовьте инфраструктуру

--- src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -1,2 +1,41 @@
-package ru.stqa.training.selenium;public class MyFirstTest {
+package ru.stqa.training.selenium;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @Before
+  public void start() {
+    driver = new FirefoxDriver();
+    wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+  }
+
+  @Test
+  public void myFirstTest() {
+    driver.get("https://www.google.com");
+    driver.findElement(By.name("q")).sendKeys("webdriver" + Keys.ENTER);
+    wait.until(titleIs("webdriver - Поиск в Google"));
+  }
+
+
+  @After
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
 }
