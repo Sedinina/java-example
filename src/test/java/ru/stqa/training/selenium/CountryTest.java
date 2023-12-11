e02c7643d4d593463091cc359568e0a895802e0e
Sedinina <sedinina.k@yandex.ru>
2023-12-11 08:51:40 +0500

    Задание 9. Проверить сортировку геозон на странице геозон

--- src/test/java/ru/stqa/training/selenium/CountryTest.java
+++ src/test/java/ru/stqa/training/selenium/CountryTest.java
@@ -16,10 +16,15 @@ public class CountryTest extends TestBase {
 
   List<WebElement> rowZones;
 
-  public void table() {
+  public void tableCountry() {
     rowNameCountry = driver.findElements(By.cssSelector("tr.row td:nth-of-type(5)"));
     rowZones = driver.findElements(By.cssSelector("tr.row td:nth-of-type(6)"));
   }
+
+  public void tableZone() {
+    rowNameCountry = driver.findElements(By.cssSelector("table.dataTable td:nth-of-type(3)"));
+  }
+
 
   @Test
   public void sortingCountriesTest() {
@@ -28,7 +33,7 @@ public class CountryTest extends TestBase {
     loginTest.loginPageObjectTest();
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
 
-    table();
+    tableCountry();
     int size = rowNameCountry.size();
     System.out.println(size);
     for (int i = 0; i < rowNameCountry.size() - 1; i++) {
@@ -51,14 +56,43 @@ public class CountryTest extends TestBase {
           String zoneNameNext = allZones.get(j + 1).getText();
           System.out.println(zoneName);
           if (zoneNameNext.compareTo(zoneName) > 0) {
-            continue;
           }
-          //Assert.assertTrue(zoneNameNext.compareTo(zoneName)>0);
+
         }
         driver.navigate().back();
-        table();
+        tableCountry();
       }
     }
   }
+
+  @Test
+  public void sortingZoneTest() {
+    loginTest = new LoginTest();
+    loginTest.loginPageObjectTest();
+    driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+    tableZone();
+    int size = rowNameCountry.size();
+
+    for (int i = 0; i < size; i++) {
+      String countryName = rowNameCountry.get(i).getText();
+      driver.findElement(By.linkText(countryName)).click();
+
+      List<WebElement> row = driver.findElements(By.cssSelector("[name*=zone_code]"));
+      int sizeZone = row.size();
+      System.out.println(sizeZone);
+
+      for (int j = 0; j < sizeZone - 1; j++) {
+
+        String zoneName = row.get(j).findElement(By.cssSelector("option[selected=selected]")).getText();
+        String nextZoneName = row.get(j + 1).findElement(By.cssSelector("option[selected=selected]")).getText();
+        System.out.println(zoneName);
+        Assert.assertTrue(nextZoneName.compareTo(zoneName) > 0);
+      }
+      driver.findElement(By.cssSelector("button[name=cancel]")).click();
+      tableZone();
+    }
+  }
+
 }
+
 
